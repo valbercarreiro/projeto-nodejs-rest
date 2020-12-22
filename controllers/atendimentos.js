68ebc67d4ba1a044045ab7b2dddd8363531c07ef
Valber Carreiro <valbercarreiro@gmail.com>
2020-12-22 10:22:15 -0300

    criando novas rotas e configurações

---
+++ controllers/atendimentos.js
@@ -0,0 +1,8 @@
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Você está na roda de atendimentos e está realizando um POST')
+    })
+}
