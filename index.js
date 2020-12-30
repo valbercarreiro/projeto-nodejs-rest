1cf65585d8613b459028780a2189eb670f6486e0
Valber Carreiro <valbercarreiro@gmail.com>
2020-12-30 09:55:37 -0300

    criando conexao

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/Tabelas')
 
-const app = customExpress()
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log('conectado com sucesso')
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+        Tabelas.init(conexao)
+        
+        const app = customExpress()
+
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    }
+})
