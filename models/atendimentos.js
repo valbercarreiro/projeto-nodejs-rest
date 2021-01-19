d1cb5c8d7a880d258834477e00aa9ee7fdf0da44
Valber Carreiro <valbercarreiro@gmail.com>
2021-01-19 11:47:15 -0300

    ajustando validacoes

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,16 +1,48 @@
+const moment = require('moment')
+
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
-    adiciona(atendimento) {
-        const sql = 'INSERT INTO ATENDIMENTOS SET ?'
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
-            if(erro) {
-                console.log(erro)
-            } else {
-                console.log(resultados)
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dateEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos 5 caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+            const sql = 'INSERT INTO ATENDIMENTOS SET ?'       
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    console.log(erro)
+                    res.status(400).json(erro)
+                } else {
+                    console.log(resultados)
+                    res.status(201).json(resultados)
+                }
+            })
+        }
+
     }
 }
 
