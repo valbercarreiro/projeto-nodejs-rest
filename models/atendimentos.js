e365da39cf3cca7abd9da23d800bc01289ea363f
Valber Carreiro <valbercarreiro@gmail.com>
2021-01-20 14:47:26 -0300

    add novas funcionalidades

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -38,11 +38,65 @@ class Atendimento {
                     res.status(400).json(erro)
                 } else {
                     console.log(resultados)
-                    res.status(201).json(resultados)
+                    res.status(201).json(atendimento)
                 }
             })
         }
 
+    }
+
+    lista(res) {
+        const sql = 'SELECT * FROM Atendimentos'
+
+        conexao.query(sql, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    buscaPorId(req, res) {
+        const sql = 'SELECT * FROM Atendimentos WHERE id =${id}'
+
+        const atendimento = resultado[0]
+
+        conexao.query(sql, (erro, resultado) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(atendimento)
+            }
+        })
+    }
+
+    altera(id, valores, res) {
+        if(valores.data) {
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        }
+
+        const sql = 'UPDATE Atendimentos SET ? WHERE id = ?'
+
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({...valores, id})
+            }
+        })
+    }
+
+    deleta(id, res) {
+        const sql = 'DELETE FROM Atendimentos WHERE id = ?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
+            }
+        })
     }
 }
 
