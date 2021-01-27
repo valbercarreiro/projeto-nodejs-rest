31a27407bdab156e76e04359dc14b08ed941ea5e
Valber Carreiro <valbercarreiro@gmail.com>
2021-01-27 15:22:16 -0300

    salvando arquivos

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,5 +1,20 @@
 const fs = require('fs')
+const path = require('path')
 
-fs.createReadStream('../assets/salsicha.jpg')
-    .pipe(fs.createWriteStream('../assets/salsicha-stream.jpg'))
-    .on('finish', () => console.log('Imagem escrita com sucesso'))
+module.exports = (caminho, nomeDoArquivo, callBackImagemCriada) => {
+    const tiposValidos = ['jpg', 'png', 'jpeg']
+    const tipo = path.extname(caminho)
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) !== -1
+
+    if(tipoEhValido) {
+        const novoCaminho = `../assets/imagens/${nomeDoArquivo}${tipo}`
+
+        fs.createReadStream(caminho)
+            .pipe(fs.createWriteStream(novoCaminho))
+            .on('finish', () => callBackImagemCriada(false, novoCaminho))
+    } else {
+        const erro = 'Tipo é inválido'
+        console.log('Erro! Tipo invalido')
+        callBackImagemCriada(erro)
+    }
+}
